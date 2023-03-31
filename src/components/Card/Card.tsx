66d902e2bbd4ed9afd1a10f87999857c75778b92
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2023-03-31 19:12:54 -0300

    Finish project

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -11,11 +11,17 @@ export type CardProps = {
 const Card: React.FC<CardProps> = ({ image, title, desc }) => {
     return (
         <div className="flex flex-col items-center md:items-start gap-24">
-            <img className="w-32 h-32" src={image} alt={title} />
+            <img
+                className="w-32 h-32 md:w-60 md:h-60"
+                src={image}
+                alt={title}
+            />
             <h3 className="text-dark-blue text-[2.8rem] md:text-[3rem]">
                 {title}
             </h3>
-            <p className="text-grayish-blue text-center md:text-left text-base md:text-lg ">{desc}</p>
+            <p className="text-grayish-blue text-center md:text-left text-base md:text-lg ">
+                {desc}
+            </p>
         </div>
     );
 };
